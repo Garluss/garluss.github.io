b3732f5df1a10ab8d7d4ba178d88b577de5a4a9a
Daniel Olsen <daniel.boenes@gmail.com>
2025-10-17 10:23:47 +0200

    laget knapp på test-side for js

--- Tester/JS1/script.js
+++ Tester/JS1/script.js
@@ -1,28 +1,14 @@
-// Dette er en kommentar
-// Variabler
-let nummer = 4.45323;
-let tekst = "Hallo";
-let ja = true;
-const Gud = true;
-
-let avstand = 50;
-
-let liste = ["funny1.png","icons/icon.ico"];
-
-// Utskrift
-console.log(`${avstand}m`);
-
-console.warn("!!! -- Advarsel -- !!!");
-console.error("Error 601: ");
-
-//alert("Bøøøøøø!");
-
-//Bedre å bruke .innerText i dette tilfellet
-const elementUt = document.getElementById("js-utskrift");
-elementUt.innerHTML = "<p>Meg: Hva er din alder?</p>";
-
-let input = prompt("Hva er din alder?");
-elementUt.innerHTML += `<p> Du: ${input} </p>`;
-
-let årstall = new Date().getFullYear();
-elementUt.innerHTML += `<p> Meg: Da er du født i ${Number(årstall) - Number(input)}</p>`;
+let lys = true;
+document.getElementById("knapp").addEventListener("click",lysPåAv);
+function lysPåAv() {
+    if (lys == true) {
+        document.getElementById('stylelink').setAttribute('href',"dark.css");
+        document.getElementById("knapp").innerText = "Lys på";
+        lys = false;
+    } 
+    else {
+        document.getElementById('stylelink').setAttribute('href',"light.css");
+        document.getElementById("knapp").innerText = "Lys av";
+        lys = true;
+    }
+}
