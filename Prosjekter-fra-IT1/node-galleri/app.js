212dc900751aa4ccdf4b3fbec29229caeb719bc3
Daniel Olsen <daniel.boenes@gmail.com>
2025-11-05 15:49:46 +0100

    prøvder meg videre på nodejs

--- Prosjekter-fra-IT1/node-galleri/app.js
+++ Prosjekter-fra-IT1/node-galleri/app.js
@@ -2,7 +2,20 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+const fs = require("fs");
+
 app.use(express.static(__dirname + '/public'));
+
+app.get("/api/bilder", (req,res) => {
+    fs.readdir(__dirname + "/public/bilder", (_,filer) => {
+        res.json(filer);
+    });
+});
+
+app.post("/api/opplast", (req, res) => {
+    console.log(req.body.bildeFil);
+});
+
 
 app.listen(port, () => console.log(`Running on ${port}`));
 
--- Prosjekter-fra-IT1/node-galleri/public/script.js
+++ Prosjekter-fra-IT1/node-galleri/public/script.js
@@ -1,1 +1,38 @@
 //legg til get og give greier her og i app.js slik at han kan hente bildene og sende bilder
+
+async function hentData() {
+    const response = await fetch("/api/bilder", { method: "GET" });
+    const data = await response.json();
+
+    main(data);
+}
+function main(data) {
+    const bilder = data;
+    const knappN = document.querySelector("#neste");
+    const knappF = document.querySelector("#forrige");
+
+    let bilde_indeks = 0;
+
+    knappN.addEventListener("click", (event) => {
+        if (bilde_indeks+1 >= Object.keys(bilder).length) {
+            bilde_indeks = 0;
+        } else {
+            bilde_indeks += 1;
+        }
+        visBilde(bilder[bilde_indeks]);
+    });
+    knappF.addEventListener("click", (event) => {
+        if (bilde_indeks-1 < 0) {
+            bilde_indeks = Object.keys(bilder).length-1;
+        } else {
+            bilde_indeks += -1;
+        }
+        visBilde(bilder[bilde_indeks]);
+    });
+    visBilde(bilder[bilde_indeks]);
+}
+function visBilde(bilde) {
+    const ramme = document.querySelector("#visning");
+    ramme.setAttribute("src","/bilder/"+bilde);
+}
+hentData();
