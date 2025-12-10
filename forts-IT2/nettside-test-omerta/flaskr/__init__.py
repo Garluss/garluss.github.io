af53436c39cdaba68f4c261f9e45fff852e81cf1
Garluss <daniel.boenes@gmail.com>
2025-12-10 20:36:36 +0100

    ordner templates for nettside

--- forts-IT2/nettside-test-omerta/flaskr/__init__.py
+++ forts-IT2/nettside-test-omerta/flaskr/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,7 +6,7 @@ app.secret_key = '_test46'
 
 @app.route("/")
 def home():
-    return "Hello"
+    return render_template("base.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
